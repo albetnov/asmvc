586f802177982c432a3a7684abcb12d637a96e1c
albetnovendo <ryxinfrvr@gmail.com>
2022-02-03 11:17:55 +0700

    add comments

--- App/Router/url.php
+++ App/Router/url.php
@@ -6,13 +6,14 @@ use Albet\Asmvc\Core\Route;
  * You can use following method for routing:
  * Route::add($urlPath, [Controller::class, 'methodName'], $HttpMethod, $Middleware).
  * Route::inline($urlPath, $CallableFunction, $httpMethod, $Middleware).
+ * Route::view($path, [$view, $data], $httpMethod, $Middleware). Note: Array is not necessary if you not using $data.
  * $httpMethod and $middleWare can be optional.
  * It can either be
  * Route::add($urlPath, [Controller::class, 'methodName'], $HttpMethod) for Http method only
  * or
  * Route::add($urlPath, [Controller::class, 'methodName'], $Middleware) for Middleware only.
  * or both of them.
- * The same rules applies for inline. 
+ * The same rules applies for inline and view. 
  */
 
 
