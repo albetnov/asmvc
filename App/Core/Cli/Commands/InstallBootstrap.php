879cfa8ed2120253121df1df118dc52b4f9a22aa
Albet Novendo <ryxinfrvr@gmail.com>
2022-02-15 20:37:28 +0700

    migrating bootstrap to node modules

--- App/Core/Cli/Commands/InstallBootstrap.php
+++ App/Core/Cli/Commands/InstallBootstrap.php
@@ -21,21 +21,31 @@ class InstallBootstrap extends BaseCli
      */
     public function register()
     {
-        $path = array_diff(scandir(base_path() . 'App/Core/bs5_integration/css/'), ['.', '..']);
+        if (!is_dir(base_path('node_modules'))) {
+            echo "Node Modules not detected. ASMVC will tried to run 'npm install'.\n";
+            system('npm i');
+            if (is_dir(base_path('node_modules'))) {
+                echo "Command executed successfully \n";
+            } else {
+                echo "Failed to execute npm install. Please execute it manually.\n";
+            }
+        }
+        $path = array_diff(scandir(base_path() . 'node_modules/bootstrap/dist/css/'), ['.', '..']);
         if (!is_dir(public_path() . 'css/')) {
             mkdir(public_path() . 'css/');
         }
         foreach ($path as $file) {
-            copy(base_path() . 'App/Core/bs5_integration/css/' . $file, public_path() . 'css/' . $file);
+            copy(base_path() . 'node_modules/bootstrap/dist/css/' . $file, public_path() . 'css/' . $file);
         }
-        $pathjs = array_diff(scandir(base_path() . 'App/Core/bs5_integration/js/'), ['.', '..']);
+        $pathjs = array_diff(scandir(base_path() . 'node_modules/bootstrap/dist/js/'), ['.', '..']);
         if (!is_dir(public_path() . 'js/')) {
             mkdir(public_path() . 'js/');
         }
         foreach ($pathjs as $js) {
-            copy(base_path() . 'App/Core/bs5_integration/js/' . $js, public_path() . 'js/' . $js);
+            copy(base_path() . 'node_modules/bootstrap/dist/js/' . $js, public_path() . 'js/' . $js);
         }
         $this->addBootstrap();
         echo 'Bootstrap installed successfully!' . PHP_EOL;
+        echo "Note: If bootstrap javascript failed to load. You may need to import popper js.\n";
     }
 }
