d75448b663086eef2040f03aa1a0d0fcdd26b6ae
albetnov <ryxinfrvr@gmail.com>
2022-12-28 09:42:01 +0700

    migrating more command

---
+++ App/Core/Console/Commands/ExportCore.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Asmvc\Core\Console\Commands;
+
+use App\Asmvc\Core\Console\BadgeColor;
+use App\Asmvc\Core\Console\Command;
+use App\Asmvc\Core\Console\FluentCommandBuilder;
+use App\Asmvc\Core\Console\FluentParamBuilder;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportCore extends Command
+{
+    protected function setup(FluentCommandBuilder $builder): FluentCommandBuilder
+    {
+        return $builder->setName("export:core")
+            ->setDesc("Export cores file")
+            ->addParam(
+                fn (FluentParamBuilder $pb) => $pb
+                    ->setName("type")
+                    ->setDesc("Core Type to export")
+                    ->setInputTypeRequired()
+            );
+    }
+
+    public function handler(InputInterface $inputInterface, OutputInterface $outputInterface): int
+    {
+        $type = $inputInterface->getArgument("type");
+
+        $whiteListedCore = ["errorPages"];
+
+        if (!in_array($type, $whiteListedCore)) {
+            $this->error("Invalid core files. Supported: " . join(",", $whiteListedCore));
+            return Command::FAILURE;
+        }
+
+        mkdir(base_path() . 'App/Views/Errors');
+        $list = array_diff(scandir(base_path() . 'App/Core/Errors/'), ['.', '..']);
+        foreach ($list as $file) {
+            copy(base_path() . 'App/Core/Errors/' . $file, base_path() . 'App/Views/Errors/' . $file);
+            $this->badge("Copied: $file", "INFO:", BadgeColor::Blue);
+        }
+
+        $this->success("Exported Successfully!");
+
+        return Command::SUCCESS;
+    }
+}
---
+++ App/Core/Console/Commands/InstallBootstrap.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Asmvc\Core\Console\Commands;
+
+use App\Asmvc\Core\Console\BadgeColor;
+use App\Asmvc\Core\Console\Command;
+use App\Asmvc\Core\Console\FluentCommandBuilder;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InstallBootstrap extends Command
+{
+    protected function setup(FluentCommandBuilder $builder): FluentCommandBuilder
+    {
+        return $builder->setName("install:bootstrap")
+            ->setDesc("Install Bootstrap Framework to your web app");
+    }
+
+    public function handler(InputInterface $inputInterface, OutputInterface $outputInterface): int
+    {
+        if (!is_dir(base_path('node_modules'))) {
+            $this->badge("Node Modules not detected. ASMVC will tried to run 'npm install'", "WARN", BadgeColor::Yellow);
+            system('npm i');
+            if (is_dir(base_path('node_modules'))) {
+                $this->badge("Command executed successfully", "SUCCESS", BadgeColor::Green);
+            } else {
+                $this->badge("Failed to execute npm install. Please execute it manually.", "ERROR", BadgeColor::Red);
+                return Command::FAILURE;
+            }
+        }
+        $path = array_diff(scandir(base_path() . 'node_modules/bootstrap/dist/css/'), ['.', '..']);
+        if (!is_dir(public_path() . 'css/')) {
+            mkdir(public_path() . 'css/');
+        }
+        foreach ($path as $file) {
+            copy(base_path() . 'node_modules/bootstrap/dist/css/' . $file, public_path() . 'css/' . $file);
+        }
+        $pathjs = array_diff(scandir(base_path() . 'node_modules/bootstrap/dist/js/'), ['.', '..']);
+        if (!is_dir(public_path() . 'js/')) {
+            mkdir(public_path() . 'js/');
+        }
+        foreach ($pathjs as $js) {
+            copy(base_path() . 'node_modules/bootstrap/dist/js/' . $js, public_path() . 'js/' . $js);
+        }
+
+        $this->success('Bootstrap installed successfully!');
+
+        return Command::SUCCESS;
+    }
+}
